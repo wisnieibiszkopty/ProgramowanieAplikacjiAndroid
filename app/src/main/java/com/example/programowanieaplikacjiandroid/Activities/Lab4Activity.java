dc00f4562deb443874afbbd5d153c05536a048a0
Kamil <KamilWodowski@o2.pl>
2024-04-09 12:34:09 +0200

    fixed notifications, added file downloading

--- app/src/main/java/com/example/programowanieaplikacjiandroid/Activities/Lab4Activity.java
+++ app/src/main/java/com/example/programowanieaplikacjiandroid/Activities/Lab4Activity.java
@@ -1,20 +1,32 @@
 package com.example.programowanieaplikacjiandroid.Activities;
 
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.example.programowanieaplikacjiandroid.Activities.ViewModels.Lab4ViewModel;
 import com.example.programowanieaplikacjiandroid.Data.Dto.DownloadInfo;
+import com.example.programowanieaplikacjiandroid.R;
 import com.example.programowanieaplikacjiandroid.Services.FileManagerService;
 import com.example.programowanieaplikacjiandroid.databinding.ActivityLab4Binding;
 
@@ -35,7 +47,7 @@ public class Lab4Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding  = ActivityLab4Binding.inflate(getLayoutInflater());
+        binding = ActivityLab4Binding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
 
@@ -54,13 +66,13 @@ public class Lab4Activity extends AppCompatActivity {
         super.onDestroy();
     }
 
-    private void onDownloadInfo(){
+    private void onDownloadInfo() {
         String url = binding.url.getText().toString();
 
         ExecutorService executor = Executors.newSingleThreadExecutor();
         CompletableFuture.supplyAsync(() -> {
             HttpsURLConnection connection = null;
-            try{
+            try {
                 URL urlToSend = new URL(url);
                 connection = (HttpsURLConnection) urlToSend.openConnection();
                 connection.setRequestMethod("GET");
@@ -68,7 +80,7 @@ public class Lab4Activity extends AppCompatActivity {
                 String type = connection.getContentType();
 
                 return new DownloadInfo(type, String.valueOf(size));
-            } catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             } finally {
                 if (connection != null) {
@@ -77,18 +89,36 @@ public class Lab4Activity extends AppCompatActivity {
             }
             return null;
         }, executor).thenAccept(result ->
-            runOnUiThread(() -> {
-                binding.filsize.setText(result.filesize());
-                binding.filetype.setText(result.filetype());
-        }));
+                runOnUiThread(() -> {
+                    binding.filsize.setText(result.filesize());
+                    binding.filetype.setText(result.filetype());
+                }));
     }
 
-    private void downloadFile(){
+    private void downloadFile() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Permission already granted", Toast.LENGTH_LONG).show();
+        } else {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
+            }
+        }
+
         Intent serviceIntent = new Intent(this, FileManagerService.class);
         serviceIntent.putExtra("url", binding.url.getText().toString());
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             startService(serviceIntent);
         }
+
     }
+
+    private final ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult
+        (new ActivityResultContracts.RequestPermission(), isGranted -> {
+            if(isGranted){
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show();
+            }
+        });
 
 }
--- app/src/main/java/com/example/programowanieaplikacjiandroid/Services/FileManagerService.java
+++ app/src/main/java/com/example/programowanieaplikacjiandroid/Services/FileManagerService.java
@@ -8,6 +8,7 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
@@ -19,8 +20,14 @@ import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import com.example.programowanieaplikacjiandroid.Activities.Lab4Activity;
 import com.example.programowanieaplikacjiandroid.R;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -57,7 +64,9 @@ public class FileManagerService extends Service {
                     NOTIFICATION_CHANNEL_ID,
                     NOTIFICATION_CHANNEL_NAME,
                     NotificationManager.IMPORTANCE_LOW);
+            notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
+            //notificationManager.notify(0, getNotification(0));
         }
     }
 
@@ -68,17 +77,14 @@ public class FileManagerService extends Service {
         startForeground(NOTIFICATION_ID, getNotification(0));
         Log.d(TAG, "start foregournd");
         serviceThreadHandler.post(() -> {
-            for (int time = 0; time <= 30; time++) {
-                Log.d(TAG, "startTimer()/lambda - current time: " + time);
-                updateNotification(time);
-                sendBroadcast(time);
-                downloadFile();
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            //updateNotification();
+            //sendBroadcast();
+            try{
+                downloadFile(intent.getStringExtra("url"));
+            } catch (IOException e){
+                e.printStackTrace();
             }
+
             Log.d(TAG, "Stoped service");
             mainThreadHandler.post(this::stopSelf);
         });
@@ -96,36 +102,92 @@ public class FileManagerService extends Service {
         Toast.makeText(this, "Serwis zakończył działanie", Toast.LENGTH_SHORT).show();
     }
 
-    private void downloadFile(){
-            
+    private void downloadFile(String urlString) throws IOException {
+        HttpsURLConnection conn = null;
+        DataInputStream inputStream = null;
+        FileOutputStream outputStream = null;
+        int progress = 0;
+        int fileSize = 0;
+        try {
+            Log.i("HttpConnectionService", "Rozpoczynam pobieranie");
+            URL url = new URL(urlString);
+            String fileName = urlString.substring(urlString.lastIndexOf("/"));
+            File outputFile = new File(Environment.getExternalStorageDirectory()
+                    + File.separator + "Download" + fileName);
+            if(outputFile.exists()) {
+                outputFile.delete();
+            }
+            conn = (HttpsURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            inputStream = new DataInputStream(conn.getInputStream());
+            outputStream = new FileOutputStream(outputFile.getPath());
+            byte buffer[] = new byte[1024];
+            int bytesRead;
+            fileSize = conn.getContentLength();
+            int step = 1;
+            while((bytesRead = inputStream.read(buffer, 0, 1024)) != -1) {
+                progress += bytesRead;
+                if(progress >= (fileSize/10)*step) {
+                    updateNotification(step*10);
+                    Log.i(TAG, "Postęp pobierania: " + step*10 + "/" + 100);
+                    step++;
+                    //sendProgressBroadcast(progress, fileSize, ProgressStatus.RUNNING);
+                }
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            Log.i("HttpConnectionService", "Pobieranie zakończone");
+            //downloadNotificationService.setAsCompleted();
+            //sendProgressBroadcast(progress, fileSize, ProgressStatus.COMPLETED);
+        } catch (IOException e) {
+            //sendProgressBroadcast(progress, fileSize, ProgressStatus.ERROR);
+            throw new RuntimeException(e);
+        } finally {
+            if(inputStream != null) {
+                inputStream.close();
+            }
+            if(outputStream != null) {
+                outputStream.close();
+            }
+            if(conn != null) {
+                conn.disconnect();
+            }
+        }
     }
 
-    private void sendBroadcast(int time){
+    private void sendBroadcast(){
         Intent broadcastIntent = new Intent(ACTION_BROADCAST);
-        broadcastIntent.putExtra(TIME_EXTRA, time);
+        //broadcastIntent.putExtra(TIME_EXTRA, time);
         sendBroadcast(broadcastIntent);
         Log.d(TAG, "Broadcast");
     }
 
-    private Notification getNotification(int time){
+    private Notification getNotification(int progress){
         Log.d(TAG, "start getNotification");
-        Intent intent = new Intent();
+        Intent intent = new Intent(getApplicationContext(), Lab4Activity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(),
+                0, intent, PendingIntent.FLAG_IMMUTABLE);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(
                 this, NOTIFICATION_CHANNEL_ID
-        ).setContentTitle("Tytuł")
-                .setContentText("tekst")
+        ).setContentTitle("Pobieranie pliku")
+                .setContentText("Trwa pobieranie pliku")
                 .setOngoing(true)
+                .setProgress(100, progress, false)
                 .setPriority(Notification.PRIORITY_HIGH)
                 .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pendingIntent)
                 .setTicker("Notification ticker")
-                .setWhen(System.currentTimeMillis());
+                .setWhen(System.currentTimeMillis())
+                .setChannelId(NOTIFICATION_CHANNEL_ID);
 
         return builder.build();
     }
 
-    private void updateNotification(int time){
+    private void updateNotification(int progress){
         Log.d(TAG, "start update notifitaioc");
-        notificationManager.notify(NOTIFICATION_ID, getNotification(time));
+        notificationManager.notify(NOTIFICATION_ID, getNotification(progress));
     }
 
 //    private void getHttpInfo(){
